1d5149d050c40805e740cd38d3455af70d25dc25
BuildTools <unconfigured@null.spigotmc.org>
2025-11-16 22:50:02 +0200

    batters

---
+++ chapters/03_data_transformation/batters.R
@@ -0,0 +1,16 @@
+batters <- Lahman::Batting |> 
+  group_by(playerID) |> 
+  summarize(
+    performance = sum(H, na.rm = TRUE) / sum(AB, na.rm = TRUE),
+    n = sum(AB, na.rm = TRUE)
+  )
+batters
+
+batters |> 
+  filter(n > 100) |> 
+  ggplot(aes(x = n, y = performance)) +
+  geom_point(alpha = 1 / 10) + 
+  geom_smooth(se = FALSE)
+
+batters |> 
+  arrange(desc(performance))
